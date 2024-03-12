6d134d4d7c318e5366d9203682c2132bc3988df7
spammyg413 <spamguinto@gmail.com>
2024-03-12 12:49:03 -0700

    fixed some css of the navbar and logo

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -43,22 +43,41 @@ const Header = () => {
       <Container>
         <Row>
           <Col lg='4' md='3' sm='4'>
-            <div>
-            <h1><Link to='/home'>
+            <div className="logo">
+            <h1>
+              <Link to='/home' class="d-flex align-items-center gap-3 ">
                 <i class="ri-car-line"></i>
-              <span>Rent<br /> N <br />Run</span>
+              <span> Rent <br/>N Run</span>
             </Link></h1>
             </div>
           </Col>
+
           <Col lg='3' md='4' sm='4'>
             <div className="header__location">
               <span ><i class="ri-earth-line"></i></span>
+              <div className="header__location-content">
+                <h4>Los Angeles</h4>
+                <h6>California</h6>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg='3' md='4' sm='4'>
+            <div className="header__location">
+              <span ><i class="ri-time-line"></i></span>
               <div className="header__location-content">
                 <h4>Monday to Friday</h4>
                 <h6>8am - 6pm</h6>
               </div>
             </div>
-          
+          </Col>
+
+          <Col lg='2' md='3' sm='0'>
+            <button className="header__btn btn d-flex align-items-center justify-content-end text-end">
+              <Link to = '/contact'>
+              <i class="ri-phone-line"></i> Request a call
+              </Link>
+            </button>
           </Col>
         </Row>
       </Container>
