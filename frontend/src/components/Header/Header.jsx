c4a275dc709706ade4a2e1349265b555b913ea64
spammyg413 <spamguinto@gmail.com>
2024-03-12 21:28:45 -0700

    fixed css for header and am adding navLink bar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,23 @@
 /* eslint-disable react/no-unknown-property */
 // eslint-disable-next-line no-unused-vars
 import { Container, Row, Col,} from "reactstrap";
-import { Link } from 'react-router-dom';
+import { Link,NavLink } from 'react-router-dom';
 import "../../styles/header.css";
+
+const navLinks =[
+  {
+    path:'/home',  
+    display:'Home'
+  },
+  {
+    path:'/about',
+    display:'About'
+  },
+  {
+    path:'/cars',
+    display:'Cars'
+  },
+]
 
 const Header = () => {
   return (
@@ -40,46 +55,80 @@ const Header = () => {
     {/*==========================Header Middle================================*/}
 
     <div className="header__middle">
-      <Container>
-        <Row>
-          <Col lg='4' md='3' sm='4'>
-            <div className="logo">
-            <h1>
-              <Link to='/home' class="d-flex align-items-center gap-3 ">
-                <i class="ri-car-line"></i>
+  <Container>
+    <Row>
+      <Col lg={4} md={3} sm={4}>
+        <div className="logo">
+          <h1>
+            <Link to='/home' className="d-flex align-items-center justify-content-left gap-3">
+              <i className="ri-car-line"></i>
               <span> Rent <br/>N Run</span>
-            </Link></h1>
-            </div>
-          </Col>
+            </Link>
+          </h1>
+        </div>
+      </Col>
 
-          <Col lg='3' md='4' sm='4'>
-            <div className="header__location">
-              <span ><i class="ri-earth-line"></i></span>
-              <div className="header__location-content">
-                <h4>Los Angeles</h4>
-                <h6>California</h6>
-              </div>
-            </div>
-          </Col>
+      <Col lg={3} md={4} sm={4}>
+        <div className="header__location d-flex align-items-center gap-2">
+          <span ><i className="ri-earth-line"></i></span>
+          <div className="header__location-content">
+            <h4>Los Angeles</h4>
+            <h6>California</h6>
+          </div>
+        </div>
+      </Col>
 
-          <Col lg='3' md='4' sm='4'>
-            <div className="header__location">
-              <span ><i class="ri-time-line"></i></span>
-              <div className="header__location-content">
-                <h4>Monday to Friday</h4>
-                <h6>8am - 6pm</h6>
-              </div>
-            </div>
-          </Col>
+      <Col lg={3} md={4} sm={4}>
+        <div className="header__location d-flex align-items-center gap-2">
+          <span ><i className="ri-time-line"></i></span>
+          <div className="header__location-content ">
+            <h4>Monday to Friday</h4>
+            <h6>8am - 6pm</h6>
+          </div>
+        </div>
+      </Col>
 
-          <Col lg='2' md='3' sm='0'>
-            <button className="header__btn btn d-flex align-items-center justify-content-end text-end">
-              <Link to = '/contact'>
-              <i class="ri-phone-line"></i> Request a call
-              </Link>
-            </button>
-          </Col>
-        </Row>
+      <Col lg={2} md={3} sm={0}>
+        <button className="header__btn btn d-flex align-items-center justify-content-end text-end">
+          <Link to='/contact'>
+            <i className="ri-phone-line"></i> Request a call
+          </Link>
+        </button>
+      </Col>
+    </Row>
+  </Container>
+</div>
+
+
+{/*============= main navigation ===============*/}
+    <div className="main__navbar">
+      <Container>
+        <div className="navigation__wrapper d-flex 
+                        align-items-center justify-content-between">
+          <span className="mobile__menu">
+          <i class="ri-menu-line"></i>
+          </span>
+
+          <div className="navigation">
+            <div className="menu">
+              {
+                navLinks.map((item,index)=>(
+                  <NavLink to ={item.path}key={index}>{item.display}</NavLink>
+                ))
+              }
+            </div>
+          </div>
+
+          <div className="nav__right">
+            <div className="search__box">
+              <input type="text" placeholder="Search" />
+              <span>
+              <i class="ri-search-line"></i>
+              </span>
+            </div>
+          </div>
+
+        </div>
       </Container>
     </div>
 
